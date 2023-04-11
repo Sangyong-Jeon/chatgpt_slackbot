637e2d8d1edd05d3b0f1cfabac8ba92d57ba4b93
JeonSangYong <tkddyd420@naver.com>
2023-04-11 11:46:34 +0900

    init : 프로젝트 생성

---
+++ src/main/java/com/slack/chatgpt/ChatgptSlackbotApplication.java
@@ -0,0 +1,13 @@
+package com.slack.chatgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptSlackbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatgptSlackbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slack/chatgpt/ChatgptSlackbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slack.chatgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatgptSlackbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
